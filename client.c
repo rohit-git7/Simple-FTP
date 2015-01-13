56c1e279b12c411c1aa696a51a7d81a40db8a6f4
Rohit Takhar <rohit.takhar1994@gmail.com>
2015-01-13 19:51:35 +0530

    first commit

---
+++ client.c
@@ -0,0 +1,195 @@
+#include<stdio.h>
+#include<sys/types.h>//socket
+#include<sys/socket.h>//socket
+#include<string.h>//memset
+#include<stdlib.h>//sizeof
+#include<netinet/in.h>//INADDR_ANY
+#include<dirent.h> 
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+#define PORT 8000
+#define MAXSZ 1024
+
+
+int search(char file[100])
+{
+
+DIR *fd=opendir(".");
+struct dirent *entry=readdir(fd);
+while(entry != NULL)
+{
+  if(strlen(entry->d_name)==strlen(file) && strcmp(entry->d_name,file)==0 )
+    {
+     struct stat buff;
+        stat(file,&buff);
+        if(S_ISREG(buff.st_mode))
+           return 1;
+        else
+           return 0;
+     }
+entry=readdir(fd);
+}
+return 0;
+}
+
+
+
+int main(int argc, char *argv[])
+{
+int sockfd;//to create socket
+char *pass=(char *)malloc(1024);
+struct sockaddr_in serverAddress;//client will connect on this
+int n,k;
+char msg1[MAXSZ];
+char msg2[MAXSZ];
+char usr[MAXSZ];
+
+
+//create socket
+sockfd=socket(AF_INET,SOCK_STREAM,0);
+//initialize the socket addresses
+memset(&serverAddress,0,sizeof(serverAddress));
+serverAddress.sin_family=AF_INET;
+serverAddress.sin_addr.s_addr=inet_addr(argv[1]);
+serverAddress.sin_port=htons(PORT);
+
+//client  connect to server on port
+k=connect(sockfd,(struct sockaddr *)&serverAddress,sizeof(serverAddress));
+//send to sever and receive from server
+if(k==-1)
+{
+printf("Connection error\n");
+exit(0);
+
+}
+
+
+printf("Username:");
+scanf("%s",usr);
+pass=getpass("Please enter the password: ");
+send(sockfd,usr,strlen(usr)+1,0);
+send(sockfd,pass,strlen(pass)+1,0);
+bzero(msg1,sizeof(msg1));
+
+n=recv(sockfd,msg1,MAXSZ,0);
+
+
+if(strcmp(msg1,"true")!=0)
+{
+  printf("Authentication failure!!!\n");
+ close(sockfd);
+ exit(0);
+}
+else
+{
+char success[]="Authentication Succesful!!!";
+write(STDOUT_FILENO,success,strlen(success));
+
+
+while(1)
+{
+char cmd[100],flg[100],arg[100];
+int count=0;
+
+bzero(msg1,sizeof(msg1));
+bzero(msg2,sizeof(msg1));
+
+printf("\nftp> ");
+fflush(stdout);
+n=read(STDIN_FILENO,msg1,MAXSZ);
+
+n=strlen(msg1)+1;
+if(strcmp(msg1,"exit\n")==0)
+  {
+  printf("Closing FTP!!!\n");
+  close(sockfd); 
+  exit(1);
+   }
+else
+ {
+  char filesize[1024];
+  long long size=0,size_file=0,i=0; 
+   count=sscanf(msg1,"%s %s %s",cmd,flg,arg);
+     
+   if(strcmp(cmd,"get")==0)
+      {
+	long long int size;
+	int filehandle;
+	send(sockfd,msg1,n,0);
+	recv(sockfd,msg2,MAXSZ,0);
+	
+	if(strcmp(msg2,"found")==0)
+	{
+	
+	recv(sockfd, &size, sizeof(long long int), 0);
+	
+	char *f = malloc(size);
+	recv(sockfd, f, size, 0);
+	
+	while(1)
+	{
+	  filehandle = open(flg, O_CREAT | O_EXCL | O_WRONLY, 0666);
+	
+	   if(filehandle == -1)
+		{
+		  sprintf(strlen(flg)+flg, "%d", i);
+		}
+	   else 
+	       break;
+	}
+	
+	write(filehandle, f, size);
+	close(filehandle);
+	
+	}
+	else
+	{
+	 printf("%s\n",msg2);
+	memset(msg2,0,sizeof(msg2));
+
+	}
+	}
+	else if(strcmp(cmd,"put")==0)
+	{
+	  int filehandle = open(flg, O_RDONLY);
+	  if(filehandle == -1)
+	  {
+	   printf("No such file on the local directory\n");
+	   }
+	else
+          {
+	   int status;
+	   send(sockfd,msg1,MAXSZ,0);
+	   struct stat obj;
+	   stat(flg, &obj);
+	   size = obj.st_size;
+	   send(sockfd, &size, sizeof(int), 0);
+	   sendfile(sockfd, filehandle, NULL, size);
+	   recv(sockfd, &status, sizeof(int), 0);
+	   
+	   if(status)
+	   printf("File stored successfully\n");
+	   else
+	   printf("File failed to be stored to remote machine\n");
+
+	  }
+        }
+	else
+	{
+	send(sockfd,msg1,n,0);	
+     	n=recv(sockfd,msg2,MAXSZ,0);
+	msg2[n]='\0';
+	if(n>0)
+	printf("%s\n",msg2);
+	memset(msg2,0,sizeof(msg2));
+	}
+  } 
+
+
+}
+}
+close(sockfd);
+return 0;
+}
---
+++ server.c
@@ -0,0 +1,293 @@
+#include<stdio.h>
+#include<sys/types.h>//socket
+#include<sys/socket.h>//socket
+#include<string.h>//memset strcmp
+#include<stdlib.h>//sizeof exit
+#include<netinet/in.h>//INADDR_ANY
+#include<ctype.h> //isspace
+#include<dirent.h> // for parsing directory
+#include<unistd.h> //for access R_OK
+#include<errno.h>
+#include<sys/stat.h> //stat
+#include<fcntl.h> //O_RDONLY
+
+#include<pwd.h> //user authentication
+#include<shadow.h>
+
+char out[100];
+#define PORT 8000
+#define MAXSZ 1024
+
+
+int sys_auth_user(const char *username, const char *password)
+{
+  struct passwd *pw;
+  struct spwd *sp;
+  char *encrypted, *correct;
+
+  pw = getpwnam(username);
+  endpwent();
+
+  if (!pw) return 1; //user doesn't really exist
+
+  sp = getspnam (pw->pw_name);
+  endspent();
+  if(sp)
+     correct = sp->sp_pwdp;
+  else
+     correct = pw->pw_passwd;
+
+  encrypted = crypt(password, correct);
+  return strcmp (encrypted, correct) ? 2 : 0;  // bad pw=2, success=0
+}
+
+
+int search2(char fil[100])
+{ memset(out,0,sizeof(out));
+  if(access(fil,F_OK)==0)
+    {
+     if(access(fil,R_OK)==0)
+       {
+	struct stat buf;
+	stat(fil,&buf);
+	if(S_ISDIR(buf.st_mode))
+	   {
+		return 1;
+	     }
+	else
+	{
+	  strcpy(out,"Not a directory");
+	return -1;
+     	
+	}
+	}
+     else
+    { 
+     strcpy(out,strerror(errno));
+      return 0;
+       }
+       }
+  else
+  { 
+     strcpy(out,strerror(errno));
+     return -1;
+   }
+}
+
+
+
+int search1(char fil[100])
+{ memset(out,0,sizeof(out));
+  if(access(fil,F_OK)==0)
+    {
+     if(access(fil,R_OK)==0)
+       {
+	struct stat buf;
+	stat(fil,&buf);
+	if(S_ISREG(buf.st_mode))
+	   {
+		return 1;
+	     }
+	else
+	{
+	  strcpy(out,"Not a regular file");
+	return -1;
+     	
+	}
+	}
+     else
+    { 
+     strcpy(out,strerror(errno));
+      return 0;
+       }
+       }
+  else
+  { 
+     strcpy(out,strerror(errno));
+     return -1;
+   }
+}
+
+
+
+int main()
+{
+int sockfd;//to create socket
+int newsockfd;//to accept connection
+
+struct sockaddr_in serverAddress;//server receive on this address
+struct sockaddr_in clientAddress;//server sends to client on this address
+
+int n;
+char msg[MAXSZ];
+int clientAddressLength;
+int pid,status;
+
+//create socket
+sockfd=socket(AF_INET,SOCK_STREAM,0);
+//initialize the socket addresses
+memset(&serverAddress,0,sizeof(serverAddress));
+serverAddress.sin_family=AF_INET;
+serverAddress.sin_addr.s_addr=htonl(INADDR_ANY);
+serverAddress.sin_port=htons(PORT);
+
+//bind the socket with the server address and port
+bind(sockfd,(struct sockaddr *)&serverAddress, sizeof(serverAddress));
+
+//listen for connection from client
+listen(sockfd,5);
+
+while(1)
+{
+//parent process waiting to accept a new connection
+printf("\nWaiting for new client connection:\n");
+clientAddressLength=sizeof(clientAddress);
+newsockfd=accept(sockfd,(struct sockaddr*)&clientAddress,&clientAddressLength);
+
+//child process is created for serving each new clients
+pid=fork();
+if(pid==0)//child process rec and send
+{
+close(sockfd);
+int i=0,count,res;
+char cmd[100],flg[100],arg[100];
+char user[200],pass[200];
+n=recv(newsockfd,user,200,0); //user authentication
+n=recv(newsockfd,pass,200,0);
+
+int auth=sys_auth_user(user,pass);
+if(auth==0)
+{
+char success[]="true";
+n=strlen(success)+1;
+send(newsockfd,success,n,0);
+//receive from client
+while(1)
+{
+count=0;
+
+n=recv(newsockfd,msg,MAXSZ,0);
+//printf("%s",msg);
+
+
+count=sscanf(msg,"%s %s %s",cmd,flg,arg);
+
+if(strcmp(cmd,"pwd")==0)
+{
+	char curr_dir[1024];
+	getcwd(curr_dir,sizeof(curr_dir));
+	send(newsockfd,curr_dir,strlen(curr_dir),0);
+}
+else if(strcmp(cmd,"ls")==0)
+{
+	char def1[1024];
+	int num;
+	system("ls > /tmp/new.txt");
+	memset(def1,'\0',sizeof(def1));
+	int fd=open("/tmp/new.txt",O_RDONLY);
+	
+	if((num=read(fd,def1,1024)) >0)
+	  {
+	  write(newsockfd,def1,num);
+	 memset(def1,0,sizeof(def1));
+
+	   }
+	close(fd);
+}
+else if(strcmp(cmd,"cd")==0)
+{
+	res=search2(flg);
+	if(res!=1)
+	{
+	send(newsockfd,out,strlen(out),0);
+	}
+	else
+	{
+	char def[]="Done";
+	 chdir(flg);
+	 send(newsockfd,def,strlen(def),0);
+	}
+}
+else if(strcmp(cmd,"get")==0)
+{
+	char content[1024],size[1024];
+	bzero(content,sizeof(content));
+	char warn[]="File not present!!!";
+	char found[]="found";
+	char deny[]="Permission denied";
+	res=search1(flg);
+  
+	if(res==1)
+     	{
+     	send(newsockfd,found,strlen(found),0); 
+	
+	long long int size;
+	 struct stat obj;
+	 stat(flg,&obj);
+	  int filehandle = open(flg, O_RDONLY);
+ 	size = obj.st_size;
+ 	
+	if(filehandle == -1)
+ 	   size = 0;
+ 	
+	send(newsockfd, &size, sizeof(long long int), 0);
+ 	if(size)
+	 sendfile(newsockfd, filehandle, NULL, size);
+	
+	close(filehandle);
+	      }
+    	else
+     	{
+     	send(newsockfd,out,strlen(out),0); 
+      	}
+	
+}
+else if(strcmp(cmd,"put")==0)
+{
+	 int c = 0, len,filehandle,size;
+	 char *f;
+	 recv(newsockfd, &size, sizeof(int), 0);
+	 int i = 1;
+	 while(1)
+	 {
+	 filehandle = open(flg, O_CREAT | O_EXCL | O_WRONLY, 0666);
+	 if(filehandle == -1)
+	 {
+	 sprintf(flg + strlen(flg), "%d", i);
+	 }
+	 else
+	   break;
+	 }
+	 f = malloc(size);
+	 recv(newsockfd, f, size, 0);
+	 c = write(filehandle, f, size);
+	 close(filehandle);
+	 send(newsockfd, &c, sizeof(int), 0);
+
+}
+else
+{
+	msg[n]='\0';
+	send(newsockfd,msg,n,0);
+}
+
+}//close interior while
+exit(0);
+}
+else
+{
+char fail[]="Authentication failure";
+n=strlen(fail);
+send(newsockfd,fail,n,0);
+close(newsockfd);
+}
+}
+else
+{
+//wait(status);
+close(newsockfd);//sock is closed BY PARENT
+}
+}//close exterior while
+
+return 0;
+}
